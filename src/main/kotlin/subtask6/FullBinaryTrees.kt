361334515a6fc9a2192576594af3162500aff224
Dremlin <as.bliznets@gmail.com>
2020-05-06 20:44:22 +0300

    feat: complete task 6

--- src/main/kotlin/subtask6/FullBinaryTrees.kt
+++ src/main/kotlin/subtask6/FullBinaryTrees.kt
@@ -1,5 +1,7 @@
 package subtask6
 
+import java.util.*
+
 /*
 ### 6) Full Binary Trees
 A full binary tree is a binary tree where each node has exactly 0 or 2 children.
@@ -18,7 +20,6 @@ Output:
 [0,0,0,0,0,null,null,0,0]
 ]
 
-
 For a visual explanation on the output have a look at the following example:
 <img alt="example trees" src="/images/ExampleTrees.png" />
 The order of images vs output lines is from left to right, from top to bottom.
@@ -26,8 +27,80 @@ Note: trailing null nodes are omitted from the output.
 */
 class FullBinaryTrees {
 
-    // TODO: Complete the following function
     fun stringForNodeCount(count: Int): String {
-        throw NotImplementedError("Not implemented")
+        val binaryTreeCombinations = createAllBinaryTreeCombination(count)
+        val res = mutableListOf<MutableList<Int?>>()
+        for (node in binaryTreeCombinations) {
+            val stateOfNode = createStatesOfNode(node!!)
+            while (stateOfNode[stateOfNode.size - 1] == null) {
+                stateOfNode.removeAt(stateOfNode.size - 1)
+            }
+            res.add(stateOfNode)
+        }
+        return res.toString()
+    }
+
+    private fun createAllBinaryTreeCombination(count: Int): ArrayList<TreeNode?> {
+        if (count == 1) return arrayListOf(TreeNode(0))
+        if (count % 2 == 0) return ArrayList()
+        val list = ArrayList<TreeNode?>()
+        for (i in 0 until count) {
+            for (left in createAllBinaryTreeCombination(i)) {
+                for (right in createAllBinaryTreeCombination(count - i - 1)) {
+                    val node = TreeNode(0);
+                    node.left = left;
+                    node.right = right;
+                    list.add(node);
+                }
+            }
+        }
+        return list;
+    }
+
+    private fun createStatesOfNode(node: TreeNode): ArrayList<Int?> {
+        var level = 0
+        val resultArray = arrayListOf<Int?>(0)
+        val nodeChildren = arrayListOf(node.left, node.right)
+        while (nodeChildren.isNotEmpty()) {
+            val grandChildren = arrayListOf<TreeNode?>();
+            for (child in nodeChildren) {
+                if (child == null && level != node.value) {
+                    resultArray.add(null);
+                } else if (child != null) {
+                    resultArray.add(0);
+                    grandChildren.add(child.left);
+                    grandChildren.add(child.right);
+                }
+            }
+            level++;
+            nodeChildren.clear();
+            nodeChildren.addAll(grandChildren);
+            grandChildren.clear();
+        }
+        return resultArray;
+    }
+
+    class TreeNode {
+        var value: Int = 0
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+
+        constructor() {}
+        constructor(value: Int) {
+            this.value = value
+        }
+
+        constructor(value: Int, left: TreeNode, right: TreeNode) {
+            this.value = value
+            this.left = left
+            this.right = right
+        }
+
+        fun hasChildren(): Boolean {
+            if (left == null && right == null) {
+                return false
+            }
+            return true
+        }
     }
 }
